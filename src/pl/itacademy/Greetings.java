eaa63a1e82d433b619e0e0039ab1b78f087dfc5a
olegmiercoles <48553027+olegmiercoles@users.noreply.github.com>
2021-04-10 19:13:15 +0200

    Update Greetings.java
    Add !

--- src/pl/itacademy/Greetings.java
+++ src/pl/itacademy/Greetings.java
@@ -3,7 +3,7 @@ package pl.itacademy;
 public class Greetings {
     public static void main(String[] args) {
         if(args.length != 0) {
-            System.out.printf("Hello, %s%n", args[0]);
+            System.out.printf("Hello, %s!%n", args[0]);
         } else {
             System.out.println("Hello there!");
         }
